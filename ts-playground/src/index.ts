8e3269f40b515510195438c1a491c8c9362c236a
Devansh Singh Kushwah <devanshs843@gmail.com>
2025-05-25 00:13:13 +0530

    Completed Typescript module

--- ts-playground/src/index.ts
+++ ts-playground/src/index.ts
@@ -1,1 +1,12 @@
-console.log(document);
+import express, {Request, Response} from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (request: Request, response: Response) => {
+    response.send("Hello, Typescript with Express!");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on the port: ${port}`);
+});
