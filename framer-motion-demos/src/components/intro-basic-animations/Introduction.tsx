28346f9d0d3cfea2b318c2967dd8c0ae0f817ce4
Devansh Singh Kushwah <devanshs843@gmail.com>
2025-06-03 13:04:08 +0530

    Framer Motion: Transformations completed

--- framer-motion-demos/src/components/intro-basic-animations/Introduction.tsx
+++ framer-motion-demos/src/components/intro-basic-animations/Introduction.tsx
@@ -3,10 +3,25 @@ import { motion } from "motion/react";
 const Introduction = () => {
     return (
         <div>
-            <motion.div 
-                className="box" 
-                animate={{x: 0}}
-            />
+            {/* Transforming Position */}
+            {/* <motion.div className="box" animate={{ x: 100 }} /> */}
+            {/* <motion.div className="box" animate={{ y: 100 }} /> */}
+
+            {/* Transforming Rotation */}
+            {/* <motion.div className="box" animate={{ rotateX: 60 }} /> */}
+            {/* <motion.div className="box" animate={{ rotateY: 60 }} /> */}
+            {/* <motion.div className="box" animate={{ rotate: 20 }} /> */}
+
+            {/* Transforming Scale */}
+            {/* <motion.div className="box" animate={{ scaleX: 2 }} /> */}
+            {/* <motion.div className="box" animate={{ scaleY: 2 }} /> */}
+            {/* <motion.div className="box" animate={{ scale: 2 }} /> */}
+
+
+            {/* Transforming Tilt or Skew */}
+            {/* <motion.div className="box" animate={{ skewX: 20 }} /> */}
+            {/* <motion.div className="box" animate={{ skewY: 20 }} /> */}
+            <motion.div className="box" animate={{ skew: 30 }} />
         </div>
     )
 }
